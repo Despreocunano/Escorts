0a1486319692750a7a77fcbc92b94d141f855797
Despreocunano <hola@sorostica.com>
2025-03-18 00:21:44 -0300

    sad

--- src/components/ModelList.tsx
+++ src/components/ModelList.tsx
@@ -81,6 +81,8 @@ export default function ModelList() {
           <img
             src={model.main_image}
             alt={model.name}
+            loading="lazy"
+            decoding="async"
             className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-110"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500">
