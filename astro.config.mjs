8f346be873b33e3bb9955fce594ef39a2de344e3
Despreocunano <hola@sorostica.com>
2025-03-20 00:53:02 -0300

    sd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static',
-  adapter: vercel({
-    analytics: true,
-  }),
+  output: 'server',
+  adapter: vercel()
 });
